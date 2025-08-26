eced7132ca8911eb0154b4cfe9f6fa790d251d8e
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-08-26 19:01:50 +0700

    created config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+from configparser import ConfigParser
+
+ROOT_DIR = os.path.dirname(__file__)
+
+DATA_DIR = os.path.join(ROOT_DIR, "data")
+
+LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+
+def config(filename: str="database.ini", section: str="postgresql") -> dict:
+    """Возвращает параметры для подключения к БД"""
+    parser = ConfigParser()
+
+    parser.read(os.path.join(ROOT_DIR, filename), encoding='utf-8')
+
+    if not parser.has_section(section):
+        raise Exception(f"Section {section} is not found in the {filename} file")
+
+    return {key: value.strip() for key, value in parser.items(section)}
