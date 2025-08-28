97490dd98b5df234d45a18a5bf29d8349e72ba57
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-08-28 18:06:28 +0700

    created src/hh_db_manager.py

--- src/hh_db.py
+++ src/hh_db.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import psycopg2
 
 from config import config
@@ -6,36 +8,34 @@ from src.logging_config import LoggingConfigClassMixin
 
 class HeadHunterDataBase(LoggingConfigClassMixin):
     """Класс для создания базы данных с компаниями и вакансиями сайта HeadHunter.ru"""
-    def __init__(self) -> None:
+    def __init__(self, dbname: Optional[str]) -> None:
         """Конструктор класса"""
         super().__init__()
-        self.base_dbname = "postgres"
-        self.hh_dbname = "headhunter_vacancies"
-        self.params = self.get_params()
+        self._base_dbname = "postgres"
+        self._hh_dbname = dbname if dbname else "headhunter_vacancies"
+        self._params = self.get_params()
         self.logger = self.configure()
 
-    @staticmethod
-    def get_params() -> dict:
-        """Возвращает параметры подключения из database.ini"""
-        return config()
+    def get_hh_dbname(self) -> str:
+        return self._hh_dbname
 
     def create_database(self) -> None:
         """Создает базу данных для хранения данных о компаниях и вакансиях сайта HeadHunter.ru"""
-        with psycopg2.connect(**self.params, dbname=self.base_dbname) as conn:
+        with psycopg2.connect(**self._params, dbname=self._base_dbname) as conn:
             conn.autocommit = True
             with conn.cursor() as cur:
-                cur.execute("SELECT 1 FROM pg_database WHERE datname = %s", (self.hh_dbname,))
+                cur.execute("SELECT 1 FROM pg_database WHERE datname = %s", (self._hh_dbname,))
                 is_exists = cur.fetchone()
                 if is_exists:
-                    cur.execute(f"DROP DATABASE {self.hh_dbname}")
-                    self.logger.info(f"База данных {self.hh_dbname} удалена")
-                cur.execute(f"CREATE DATABASE {self.hh_dbname}")
-                self.logger.info(f"База данных {self.hh_dbname} создана")
+                    cur.execute(f"DROP DATABASE {self._hh_dbname}")
+                    self.logger.info(f"База данных {self._hh_dbname} удалена")
+                cur.execute(f"CREATE DATABASE {self._hh_dbname}")
+                self.logger.info(f"База данных {self._hh_dbname} создана")
 
     def create_table_hh_companies(self) -> None:
         """Создает таблицу для хранения данных о компаниях сайта HeadHunter.ru"""
         try:
-            with psycopg2.connect(**self.params, dbname=self.hh_dbname) as conn:
+            with psycopg2.connect(**self._params, dbname=self._hh_dbname) as conn:
                 with conn.cursor() as cur:
                     cur.execute("""
                     CREATE TABLE hh_companies (
@@ -45,7 +45,7 @@ class HeadHunterDataBase(LoggingConfigClassMixin):
                         employer_url VARCHAR(255) NOT NULL,
 
                         CONSTRAINT pk_hh_companies_id PRIMARY KEY(company_id)
-                        )
+                        );
                     """)
             self.logger.info("Tаблица hh_companies успешно создана")
         except psycopg2.Error as e:
@@ -54,7 +54,7 @@ class HeadHunterDataBase(LoggingConfigClassMixin):
     def create_table_hh_vacancies(self) -> None:
         """Создает таблицу для хранения данных о вакансиях компаний сайта HeadHunter.ru"""
         try:
-            with psycopg2.connect(**self.params, dbname=self.hh_dbname) as conn:
+            with psycopg2.connect(**self._params, dbname=self._hh_dbname) as conn:
                 with conn.cursor() as cur:
                     cur.execute("""
                     CREATE TABLE hh_vacancies (
@@ -71,7 +71,7 @@ class HeadHunterDataBase(LoggingConfigClassMixin):
 
                         CONSTRAINT fk_hh_vacancies_company_id FOREIGN KEY(company_id) 
                         REFERENCES hh_companies(company_id) ON DELETE CASCADE
-                        )
+                        );
                     """)
             self.logger.info("Tаблица hh_vacancies успешно создана")
         except psycopg2.Error as e:
@@ -80,14 +80,14 @@ class HeadHunterDataBase(LoggingConfigClassMixin):
     def save_data_to_table_hh_companies(self, employers: list[dict]) -> None:
         """Сохранение данных о компаниях сайта HeadHunter.ru в базу данных"""
         try:
-            with psycopg2.connect(**self.params, dbname=self.hh_dbname) as conn:
+            with psycopg2.connect(**self._params, dbname=self._hh_dbname) as conn:
                 with conn.cursor() as cur:
                     for emp in employers:
                         cur.execute(
                             """
                             INSERT INTO hh_companies (hh_employer_id, employer_name, employer_url)
                             VALUES (%s, %s, %s)
-                            RETURNING company_id
+                            RETURNING company_id;
                             """,
                             (emp.get('employer_id'), emp.get('name'), emp.get('url'))
                         )
@@ -102,7 +102,7 @@ class HeadHunterDataBase(LoggingConfigClassMixin):
     def save_data_to_table_hh_vacancies(self, employers_vacancies: list[dict]) -> None:
         """Сохранение данных о вакансиях компаний сайта HeadHunter.ru в базу данных"""
         try:
-            with psycopg2.connect(**self.params, dbname=self.hh_dbname) as conn:
+            with psycopg2.connect(**self._params, dbname=self._hh_dbname) as conn:
                 with conn.cursor() as cur:
                     for emp_vacs in employers_vacancies:
                         for hh_employer_id, vacancies in emp_vacs.items():
@@ -114,17 +114,18 @@ class HeadHunterDataBase(LoggingConfigClassMixin):
                                 cur.execute(
                                     """
                                     INSERT INTO hh_vacancies 
-                                    (hh_vac_id, vac_name, vac_url, salary_from, salary_to, vac_area, company_id)
+                                    (hh_vac_id, vac_name, vac_url, company_id, vac_area, salary_from, salary_to)
                                     VALUES (%s, %s, %s, %s, %s, %s, %s)
-                                    RETURNING vacancy_id
+                                    RETURNING vacancy_id;
                                     """,
                                     (vac.get('vac_id'),
                                      vac.get('name'),
                                      vac.get('url'),
-                                     vac.get('salary_from'),
-                                     vac.get('salary_to'),
+                                     company_id,
                                      vac.get('area'),
-                                     company_id)
+                                     vac.get('salary_from'),
+                                     vac.get('salary_to')
+                                     )
                                 )
 
                     cur.execute("SELECT COUNT(*) FROM hh_vacancies")
@@ -133,3 +134,24 @@ class HeadHunterDataBase(LoggingConfigClassMixin):
 
         except psycopg2.Error as e:
             self.logger.error(f"Ошибка при добавлении данных в таблицу hh_vacancies: {e}")
+
+    def add_avg_salary_to_hh_vacancies(self) -> None:
+        """Добавление и вычисление атрибута 'average_salary' в таблице 'hh_vacancies'"""
+        with psycopg2.connect(**self._params, dbname=self._hh_dbname) as conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                ALTER TABLE hh_vacancies ADD COLUMN IF NOT EXISTS average_salary REAL;""")
+                cur.execute("""
+                UPDATE hh_vacancies
+                SET average_salary = CASE
+                    WHEN salary_from > 0 AND salary_to > 0 THEN (salary_from + salary_to) / 2
+                    WHEN salary_from = 0 THEN salary_to
+                    WHEN salary_to = 0 THEN salary_from
+                    ELSE NULL
+                END;
+                """)
+
+    @staticmethod
+    def get_params() -> dict:
+        """Возвращает параметры подключения из database.ini"""
+        return config()
---
+++ src/hh_db_manager.py
@@ -0,0 +1,86 @@
+from typing import Optional
+
+import psycopg2
+
+from config import config
+from src.logging_config import LoggingConfigClassMixin
+
+
+class HeadHunterDataBaseClient(LoggingConfigClassMixin):
+    """Класс для создания запросов к базе данных с компаниями и вакансиями сайта HeadHunter.ru"""
+    def __init__(self) -> None:
+        """Конструктор класса"""
+        super().__init__()
+        self._hh_dbname = "headhunter_vacancies"
+        self._params = self.get_params()
+        self.logger = self.configure()
+
+    def _execute_query(self, query: str | tuple, params: Optional[str] = None) -> list:
+        """Выполняет запрос к базе данных и возвращает результат"""
+        try:
+            with psycopg2.connect(**self._params, dbname=self._hh_dbname) as conn:
+                with conn.cursor() as cur:
+                    cur.execute(query, params)
+                    rows = cur.fetchall()
+            return rows  # type: ignore
+        except psycopg2.Error as e:
+            self.logger.error(f"Ошибка при выполнении запроса: {e}")
+            return []
+
+    def get_companies_and_vacancies_count(self) -> list:
+        """Получает список всех компаний и количество вакансий у каждой компании"""
+        query = """
+                SELECT c.employer_name, COUNT(v.vacancy_id) AS vacancies_count 
+                FROM hh_companies AS c
+                JOIN hh_vacancies AS v ON c.company_id = v.company_id
+                GROUP BY c.employer_name
+                ORDER BY c.employer_name;
+                """
+        return self._execute_query(query)
+
+    def get_all_vacancies(self) -> list:
+        """Получает список всех вакансий"""
+        query = """
+                SELECT c.employer_name, v.vac_name, v.salary_from, v.salary_to, vac_url
+                FROM hh_companies as c
+                JOIN hh_vacancies as v ON c.company_id = v.company_id
+                ORDER BY c.employer_name, v.vac_name;
+                """
+        return self._execute_query(query)
+
+    def get_avg_salary(self) -> list:
+        """Получает среднюю зарплату по вакансиям у каждой компании"""
+        query = """
+                SELECT c.employer_name, AVG(v.average_salary) AS average_salary
+                FROM hh_vacancies AS v
+                JOIN hh_companies AS c ON c.company_id = v.company_id
+                GROUP BY c.employer_name
+                ORDER BY average_salary DESC;
+                """
+        return self._execute_query(query)
+
+    def get_vacancies_with_higher_salary(self) -> list:
+        """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям"""
+        query = """
+                SELECT v.vac_name, c.employer_name, v.average_salary 
+                FROM hh_vacancies AS v
+                JOIN hh_companies AS c ON c.company_id = v.company_id
+                WHERE v.average_salary > (SELECT AVG(v.average_salary) FROM hh_vacancies)
+                ORDER BY average_salary DESC;
+                """
+        return self._execute_query(query)
+
+    def get_vacancies_with_keyword(self, key_word: str) -> list:
+        """Получает список всех вакансий по ключевому слову в названии"""
+        query = ("""
+                SELECT v.vac_name, c.employer_name, salary_from, salary_to, v.vac_url
+                FROM hh_vacancies AS v
+                JOIN hh_companies AS c ON c.company_id = v.company_id
+                WHERE v.vac_name ILIKE %s""",
+                 ('%' + key_word + '%',))
+        return self._execute_query(query, key_word)
+
+    @staticmethod
+    def get_params() -> dict:
+        """Возвращает параметры подключения из database.ini"""
+        return config()
