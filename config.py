01fd022b0b4672282d0c1f03c87263d70ec69d62
Nadezhda_Popova <nadezhdapopova13@yandex.ru>
2025-08-30 13:43:56 +0700

    created .env.example, modified database.ini, config.py

--- config.py
+++ config.py
@@ -1,18 +1,26 @@
 import os
 from configparser import ConfigParser
 
+from dotenv import load_dotenv
+
 ROOT_DIR = os.path.dirname(__file__)
 
 LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+load_dotenv(os.path.join(ROOT_DIR, ".env"))
 
 
 def config(filename: str = "database.ini", section: str = "postgresql") -> dict:
     """Возвращает параметры для подключения к БД"""
     parser = ConfigParser()
-
-    parser.read(os.path.join(ROOT_DIR, filename), encoding='utf-8')
+    parser.read(os.path.join(ROOT_DIR, filename), encoding="utf-8")
 
     if not parser.has_section(section):
         raise Exception(f"Section {section} is not found in the {filename} file")
 
-    return {key: value.strip() for key, value in parser.items(section)}
+    db_config = {key: value.strip() for key, value in parser.items(section)}
+
+    db_config["user"] = os.getenv("DB_USER")
+    db_config["password"] = os.getenv("DB_PASSWORD")
+
+    return db_config
