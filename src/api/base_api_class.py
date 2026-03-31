fecc3bd295e6c5787b1ed244fb1f27492a171d05
nadezhda <nadezhda13popova@gmail.com>
2026-04-01 00:18:36 +0700

    modified base api class with thread-local storage for Sesson to each thread

--- src/api/base_api_class.py
+++ src/api/base_api_class.py
@@ -1,3 +1,4 @@
+import threading
 from abc import ABC, abstractmethod
 from json import JSONDecodeError
 from typing import Optional
@@ -11,37 +12,46 @@ from src.logging_config import LoggingConfigClassMixin
 
 class BaseAPISource(ABC, LoggingConfigClassMixin):
     """Базовый класс для работы с API"""
-
     def __init__(self) -> None:
         super().__init__()
         self.logger = self.configure()
-        self.session = requests.Session()
+        self.local = threading.local()
 
-        retry = Retry(
-            total=3,  # максимум 3 попытки
-            backoff_factor=1,  # задержка между повторами
-            status_forcelist=[429, 500, 502, 503, 504],
-            allowed_methods=["GET"],
-        )
-        adapter = HTTPAdapter(max_retries=retry)  # определяет политику повторных попыток
-        self.session.mount("https://", adapter)
-        self.session.mount("http://", adapter)
+    def _get_session(self) -> requests.Session:
+        """Создаёт session отдельно для каждого потока"""
+        if not hasattr(self.local, "session"):
+            session = requests.Session()
 
-    def __enter__(self):
-        return self
+            retry = Retry(
+                total=3,  # максимум 3 попытки
+                backoff_factor=1,  # задержка между повторами
+                status_forcelist=[429, 500, 502, 503, 504],
+                allowed_methods=["GET"],
+            )
+            adapter = HTTPAdapter(  # определяет политику повторных попыток
+                max_retries=retry,
+                pool_connections=10,
+                pool_maxsize=10
+            )
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.session.close()
-        self.logger.info("HTTP session закрыта")
+            session.mount("https://", adapter)
+            session.mount("http://", adapter)
+
+            self.local.session = session
+
+        return self.local.session
 
     def _get_response(self, url: str, headers: Optional[dict] = None, params: Optional[dict] = None) -> Optional[dict]:
         """Выполняет GET запрос и возвращает JSON"""
+        session = self._get_session()
         try:
-            response = self.session.get(url, headers=headers, params=params, timeout=10)
+            response = session.get(url, headers=headers, params=params, timeout=(3, 10))  # timeout на connect, read
             response.raise_for_status()
             result = response.json()
-            self.logger.info("Данные преобразованы в json-формат")
+            self.logger.debug("JSON получен")
             return result
+        except requests.exceptions.Timeout as err:
+            self.logger.error(f"Ошибка таймаута: {err}")
         except requests.exceptions.RequestException as err:
             self.logger.error(f"Ошибка запроса: {err}")
         except JSONDecodeError as err:
